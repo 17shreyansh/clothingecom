ecc7819b81e11613d48525a4a53adb1a5e2ee0cc
Shreyansh <kake0shreyansh@gmail.com>
2025-07-23 10:34:23 +0530

    update

--- server/app.js
+++ server/app.js
@@ -101,14 +101,6 @@ app.use('/api/email-config', emailConfigRoutes);
 app.use('/api/contact', contactRoutes);
 app.use('/api/packing-time', packingTimeRoutes);
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
-}
 
 // 404 handler
 app.use('*', (req, res) => {
